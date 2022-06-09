fe67283b8d2ded746f491987725a462b8096bdac
Igor Maliy <imaliy.filsh@gmail.com>
2022-06-10 00:06:46 +0300

    Development

--- views/default/index.php
+++ views/default/index.php
@@ -13,53 +13,68 @@ $this->setTitle('System settings');
 
 ?><div class="row">
     <div class="col-12">
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3">
-            <h1 class="h3">System settings</h1>
+        <div class="card mb-3">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md">
+                        <h4 class="mb-0">System settings</h4>
+                    </div>
+                    <div class="col-auto">
+                        <div class="btn-toolbar float-right">
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
+
 <div class="mb-2"></div>
 <div class="row">
     <div class="col-12">
-        <div class="row">
-            <div class="col-3">
-                <b-list-group v-b-scrollspy="{element: '#setting-group-list', offset: 50}" style="position: sticky;top: 80px;">
-                    <?php foreach ($groupList as $index => $group) {
-                        /** @var SettingGroup $group */
-                        ?><b-list-group-item href="#setting-group-item-<?= $index ?>"><?= $group->getLabel() ?></b-list-group-item><?php
-                    } ?>
-                </b-list-group>
-            </div>
-            <div class="col-9">
-                <div id="setting-group-list">
-                    <?php foreach ($groupList as $index => $group) {
-                        /** @var SettingGroup $group */
-                        ?><h4 id="setting-group-item-1"><?= $group->getLabel() ?></h4><?php
+        <div class="card mb-3">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-3">
+                        <b-list-group v-b-scrollspy="{element: '#setting-group-list', offset: 50}" style="position: sticky;top: 80px;">
+                            <?php foreach ($groupList as $index => $group) {
+                                /** @var SettingGroup $group */
+                                ?><b-list-group-item href="#setting-group-item-<?= $index ?>"><?= $group->getLabel() ?></b-list-group-item><?php
+                            } ?>
+                        </b-list-group>
+                    </div>
+                    <div class="col-9">
+                        <div id="setting-group-list">
+                            <?php foreach ($groupList as $index => $group) {
+                                /** @var SettingGroup $group */
+                                ?><h4 id="setting-group-item-1"><?= $group->getLabel() ?></h4><?php
 
-                        $rows = [];
-                        foreach ($group as $setting) {
-                            /** @var Setting $setting */
-                            $rows[] =
-                                '<tr>'
-                                    . '<td>' . $setting->getName() . '</td>'
-                                    . '<td>' . $setting->getLabel() . '</td>'
-                                    . '<td>' . $setting->getDescription() . '</td>'
-                                    . '<td>' . $setting->getValue() . '</td>'
-                                . '</tr>';
-                        }
+                                $rows = [];
+                                foreach ($group as $setting) {
+                                    /** @var Setting $setting */
+                                    $rows[] =
+                                        '<tr>'
+                                            . '<td>' . $setting->getName() . '</td>'
+                                            . '<td>' . $setting->getLabel() . '</td>'
+                                            . '<td>' . $setting->getDescription() . '</td>'
+                                            . '<td>' . $setting->getValue() . '</td>'
+                                        . '</tr>';
+                                }
 
-                        ?><table class="table table-hover colfl-p10">
-                            <thead>
-                                <tr>
-                                    <th>Key</th>
-                                    <th>Name</th>
-                                    <th>Description</th>
-                                    <th>Value</th>
-                                </tr>
-                            </thead>
-                            <tbody><?= implode("\n", $rows) ?></tbody>
-                        </table><?php
-                    } ?>
+                                ?><table class="table table-hover colfl-p10">
+                                    <thead>
+                                        <tr>
+                                            <th>Key</th>
+                                            <th>Name</th>
+                                            <th>Description</th>
+                                            <th>Value</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody><?= implode("\n", $rows) ?></tbody>
+                                </table><?php
+                            } ?>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
